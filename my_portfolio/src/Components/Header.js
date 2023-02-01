d09781cf4be0785bf4f7a59dd22386548e5dcdbc
allnightcat <allnightcat12@gmail.com>
2023-02-01 21:46:35 +0900

    add logo & carousel

--- my_portfolio/src/Components/Header.js
+++ my_portfolio/src/Components/Header.js
@@ -6,22 +6,30 @@ const header = () => {
   return (
     <div>
       <header>
-        <div>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/my_info">내 소개</Link>
-          </li>
-          <li>
-            <Link to="/project/work">프로젝트(회사)</Link>
-          </li>
-          <li>
-            <Link to="/project/personal">프로젝트(개인)</Link>
-          </li>
-          <li>
-            <Link to="/studied">공부한 것</Link>
-          </li>
+        <div className="inner_container">
+          <div className="logo">
+            <img src="img/logos/main_logo.jpg" alt="main_logo" />
+          </div>
+
+          <div className="navigation">
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/my_info">내 소개</Link>
+            </li>
+            <li>
+              <Link to="/project/work">프로젝트(회사)</Link>
+            </li>
+            <li>
+              <Link to="/project/personal">프로젝트(개인)</Link>
+            </li>
+            <li>
+              <Link to="/studied">공부한 것</Link>
+            </li>
+          </div>
+          {/* 언어 변경 박스 */}
+          <div></div>
         </div>
       </header>
     </div>
--- my_portfolio/src/Pages/Main.js
+++ my_portfolio/src/Pages/Main.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "../CSS/Main.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Main = () => {
+  const SliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className="Main">
       <div className="banners">
-        <img src="img/banners/main_01.jpg" alt="banner1" />
-        <img src="img/banners/main_02.jpg" alt="banner2" />
-        <img src="img/banners/main_03.jpg" alt="banner2" />
+        <Slider {...SliderSettings}>
+          <div>
+            <img src="img/banners/main_01.jpg" alt="banner1" />
+          </div>
+          <div>
+            <img src="img/banners/main_02.jpg" alt="banner2" />
+          </div>
+          <div>
+            <img src="img/banners/main_03.jpg" alt="banner2" />
+          </div>
+        </Slider>
       </div>
     </div>
   );
