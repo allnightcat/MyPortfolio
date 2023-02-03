ba8180613179bb3d88008c92c18f9c30fdcddc5d
allnightcat <allnightcat12@gmail.com>
2023-02-03 21:15:12 +0900

    add Noto Sans fonts

--- my_portfolio/src/Components/Layout.js
+++ my_portfolio/src/Components/Layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
@@ -10,7 +9,6 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
-      <Footer />
     </div>
   );
 };
--- my_portfolio/src/Pages/Main.js
+++ my_portfolio/src/Pages/Main.js
@@ -1,32 +1,25 @@
 import React from "react";
 import "../CSS/Main.scss";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Main = () => {
-  const SliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
     <div className="Main">
-      <div className="banners">
-        <Slider {...SliderSettings}>
-          <div>
-            <img src="img/banners/main_01.jpg" alt="banner1" />
+      <article>
+        <section>
+          <div className="text-container">
+            <h3>안녕하세요.</h3>
+            <p>강예진입니다.</p>
           </div>
-          <div>
-            <img src="img/banners/main_02.jpg" alt="banner2" />
+          <div className="img-container">
+            <img
+              src="/img/banners/main_banner_img_01.svg"
+              alt="일하는 여자 이미지"
+            />
           </div>
-          <div>
-            <img src="img/banners/main_03.jpg" alt="banner2" />
-          </div>
-        </Slider>
-      </div>
+        </section>
+      </article>
     </div>
   );
 };
