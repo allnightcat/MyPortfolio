090f04961471d7ac2522c7d4f68b47516d32e8b4
allnightcat <allnightcat12@gmail.com>
2023-03-20 23:10:03 +0900

    태블릿 메뉴 개발 완료

--- my_portfolio/src/Components/Header.js
+++ my_portfolio/src/Components/Header.js
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react";
 
 const Header = () => {
   const [menus, setMenus] = useState(MenuDataList);
+  const [menuHamburger, setMenuHamburger] = useState(false);
   const onDisplay = useCallback(
     (id) => {
       setMenus(
@@ -28,7 +29,22 @@ const Header = () => {
     },
     [menus]
   );
+  // 태블릿 & 모바일 햄버거 메뉴 열기
+  const showMenuList = () => {
+    setMenuHamburger((item) => !item);
+  };
+  // 태블릿 & 모바일 소메뉴 열기
+  const onSubMenuDisplay = (id) => {
+    console.log(id);
+    let newMenus = menus.map((item) => {
+      if (id === item.id) {
+        item.display = !item.display;
+      }
+      return item;
+    });
 
+    setMenus(newMenus);
+  };
   return (
     <div>
       <header>
@@ -66,53 +82,65 @@ const Header = () => {
             </a>
           </div>
           <div className="hamburder-menu">
-            <div className="menu-icon">
+            <div
+              className="menu-icon"
+              style={{ cursor: "pointer" }}
+              onClick={showMenuList}
+            >
               <img src="/img/icons/mobile_menu.png " alt="메뉴 열기" />
             </div>
-            <div className="mobile-menu-list">
-              <div className="mobile-lang-box">
-                <a href="/">
-                  <img src="/img/icons/korea_flag.png" alt="korea flag" />
-                </a>
-                <a href="/">
-                  <img src="/img/icons/usa_flag.png" alt="USA flag" />
-                </a>
-                <a href="/">
-                  <img src="/img/icons/japan_flag.png" alt="Japan flag" />
-                </a>
-              </div>
-              <div className="mobile-menu-box">
-                {menus.map((menu) => {
-                  return (
-                    <div className="mobile-menu" key={menu.id}>
-                      {menu.submenu.length === 0 ? (
-                        <Link to={menu.link} className="main-menu one-menu">
-                          {menu.name}
-                        </Link>
-                      ) : (
-                        <div className="main-menu">{menu.name}</div>
-                      )}
-
-                      <div className="sub-menu">
-                        {menu.submenu.length === 0
-                          ? null
-                          : menu.submenu.map((sub_menu) => {
-                              return (
-                                <Link
-                                  to={sub_menu.sub_link}
-                                  className="menu-group"
-                                  key={sub_menu.id}
-                                >
-                                  {sub_menu.sub_name}
-                                </Link>
-                              );
-                            })}
+            {menuHamburger ? (
+              <div className="mobile-menu-list">
+                <div className="mobile-lang-box">
+                  <a href="/">
+                    <img src="/img/icons/korea_flag.png" alt="korea flag" />
+                  </a>
+                  <a href="/">
+                    <img src="/img/icons/usa_flag.png" alt="USA flag" />
+                  </a>
+                  <a href="/">
+                    <img src="/img/icons/japan_flag.png" alt="Japan flag" />
+                  </a>
+                </div>
+                <div className="mobile-menu-box">
+                  {menus.map((menu) => {
+                    return (
+                      <div className="mobile-menu" key={menu.id}>
+                        {menu.submenu.length === 0 ? (
+                          <Link to={menu.link} className="main-menu one-menu">
+                            {menu.name}
+                          </Link>
+                        ) : (
+                          <div
+                            className="main-menu"
+                            onClick={() => onSubMenuDisplay(menu.id)}
+                          >
+                            {menu.name}
+                          </div>
+                        )}
+                        {menu.display ? (
+                          <div className="sub-menu" id={"sub_" + menu.id}>
+                            {menu.submenu.length === 0
+                              ? null
+                              : menu.submenu.map((sub_menu) => {
+                                  return (
+                                    <Link
+                                      to={sub_menu.sub_link}
+                                      className="menu-group"
+                                      key={sub_menu.id}
+                                    >
+                                      {sub_menu.sub_name}
+                                    </Link>
+                                  );
+                                })}
+                          </div>
+                        ) : null}
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
-            </div>
+            ) : null}
           </div>
         </div>
       </header>
